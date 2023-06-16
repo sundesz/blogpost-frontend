3e6ba30da5eb85d352c6b5cd4dafb24f0640ca31
Sandesh Hyoju <sundesz@gmail.com>
2023-06-16 17:40:53 +0300

    Add backend url using env variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    'process.env': process.env,
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api/v1': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+// https://stackoverflow.com/questions/66389043/how-can-i-use-vite-env-variables-in-vite-config-js
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    define: {
+      'process.env': process.env,
+    },
+    server: {
+      port: 3000,
+      proxy: {
+        '/api/v1': {
+          target: `${process.env.VITE_BACKEND_URL}`,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  build: {
-    outDir: '../dist',
-  },
+    build: {
+      outDir: '../dist',
+    },
 
-  plugins: [react()],
-})
+    plugins: [react()],
+  })
+}
