76996c1415decb8f812759696ac1d1e53d818e18
Sandesh Hyoju <sundesz@gmail.com>
2023-07-02 00:05:06 +0300

    Change interface names

--- src/feature/user/SingleUser.tsx
+++ src/feature/user/SingleUser.tsx
@@ -1,11 +1,11 @@
 import { Container } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
 import PageTitle from '../../components/PageTitle';
-import { IUser } from '../../types';
+import { User } from '../../types';
 import { capitalize } from '../../utils';
 
 const SingleUser = () => {
-  let { state } = useLocation() as { state: { user: IUser } };
+  let { state } = useLocation() as { state: { user: User } };
 
   return (
     <Container className="py-5">
