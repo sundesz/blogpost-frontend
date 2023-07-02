31a3ad9405b054ade84efaf61187244b9ac7cdd5
Sandesh Hyoju <sundesz@gmail.com>
2023-07-02 08:55:28 +0300

    Add Image field

--- src/feature/signup/SignUpForm.tsx
+++ src/feature/signup/SignUpForm.tsx
@@ -1,34 +1,55 @@
 import * as Yup from 'yup';
-import { Field, Form, Formik } from 'formik';
-import { INewUser, UserRole } from '../../types';
-import { SubmitButton, InputField, SelectField, capitalize } from '../../utils';
+import { Field, Form, Formik, useFormikContext } from 'formik';
+import { NewUser, UserRole } from '../../types';
+import {
+  SubmitButton,
+  InputField,
+  SelectField,
+  capitalize,
+  FileField,
+  SUPPORTED_IMAGE_FORMATS,
+} from '../../utils';
 import PageTitle from '../../components/PageTitle';
 
-interface ISignUpFormProps {
-  onSubmit: (values: INewUser) => void;
+interface SignUpFormProps {
+  onSubmit: (values: NewUser) => void;
 }
 
-const INITIAL_VALUES = {
-  name: '',
-  email: '',
-  password: '',
-  confirmPassword: '',
-};
+const SignUpForm: React.FC<SignUpFormProps> = ({ onSubmit }) => {
+  const INITIAL_VALUES = {
+    name: '',
+    email: '',
+    password: '',
+    role: '',
+    confirmPassword: '',
+    image: null,
+  };
 
-const VALIDATION_SCHEMA = Yup.object().shape({
-  name: Yup.string().required('Name is required'),
-  email: Yup.string().email('Invalid email').required('Email is required'),
-  role: Yup.string().required('Role is required').oneOf(['user', 'author']),
-  password: Yup.string()
-    .min(6, 'Password must be at least 6 character long')
-    .max(20, 'Password should be less than 20 characters long')
-    .required('Password is required'),
-  confirmPassword: Yup.string()
-    .oneOf([Yup.ref('password'), null], "Password don't match")
-    .required('Password confirmation is required'),
-});
+  const VALIDATION_SCHEMA = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    role: Yup.string().required('Role is required').oneOf(['user', 'author']),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 character long')
+      .max(20, 'Password should be less than 20 characters long')
+      .required('Password is required'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], "Password don't match")
+      .required('Password confirmation is required'),
+    image: Yup.mixed()
+      .test(
+        'FILE_SIZE',
+        'Uploaded file is too big.',
+        (value) => !value || (value && value.size <= 1024 * 1024)
+      )
+      .test(
+        'FILE_FORMAT',
+        'Uploaded file has unsupported format.',
+        (value) =>
+          !value || (value && SUPPORTED_IMAGE_FORMATS.includes(value?.type))
+      ),
+  });
 
-const SignUpForm: React.FC<ISignUpFormProps> = ({ onSubmit }) => {
   const roleOptions = UserRole.filter((user) => user !== 'admin').map(
     (user) => ({
       name: capitalize(user),
@@ -45,54 +66,68 @@ const SignUpForm: React.FC<ISignUpFormProps> = ({ onSubmit }) => {
         validationSchema={VALIDATION_SCHEMA}
         onSubmit={onSubmit}
       >
-        <Form>
-          <Field
-            id="name"
-            label="Name"
-            name="name"
-            placeholder="Name"
-            component={InputField}
-          />
+        {(formikProps) => (
+          <Form>
+            <Field
+              id="name"
+              label="Name"
+              name="name"
+              placeholder="Name"
+              component={InputField}
+            />
 
-          <Field
-            id="email"
-            label="Email (Username)"
-            name="email"
-            placeholder="Username"
-            component={InputField}
-          />
+            <Field
+              id="email"
+              label="Email (Username)"
+              name="email"
+              placeholder="Username"
+              component={InputField}
+            />
 
-          <Field
-            name="role"
-            id="role"
-            label="Role"
-            selectOptions={roleOptions}
-            component={SelectField}
-          />
+            <Field
+              name="role"
+              id="role"
+              label="Role"
+              selectOptions={roleOptions}
+              component={SelectField}
+            />
 
-          <Field
-            id="password"
-            label="Password"
-            name="password"
-            type="password"
-            placeholder="Password"
-            component={InputField}
-          />
+            <Field
+              id="password"
+              label="Password"
+              name="password"
+              type="password"
+              placeholder="Password"
+              component={InputField}
+            />
 
-          <Field
-            id="confirmPassword"
-            label="Password confirmation"
-            name="confirmPassword"
-            type="password"
-            placeholder="Password"
-            component={InputField}
-          />
+            <Field
+              id="confirmPassword"
+              label="Password confirmation"
+              name="confirmPassword"
+              type="password"
+              placeholder="Password"
+              component={InputField}
+            />
 
-          <SubmitButton id="signup-btn" name="Sign up" />
-        </Form>
+            <Field
+              id="image"
+              label="Profile image"
+              name="image"
+              acceptType={SUPPORTED_IMAGE_FORMATS.join(',')}
+              formikProps={formikProps}
+              component={FileField}
+            />
+
+            <SubmitButton id="signup-btn" name="Sign up" />
+          </Form>
+        )}
       </Formik>
     </div>
   );
 };
 
 export default SignUpForm;
+function setFieldValue(arg0: string, file: File) {
+  throw new Error('Function not implemented.');
+}
