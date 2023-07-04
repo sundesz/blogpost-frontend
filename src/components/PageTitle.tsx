93f16090ad8906cd4c30649e0a15128afa89c3c0
Sandesh Hyoju <sundesz@gmail.com>
2023-07-04 09:11:26 +0300

    Add dynamic classes

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -2,11 +2,18 @@ import GoBack from './GoBack';
 
 interface PageTitleProps {
   title: string;
+  divClass?: string;
+  displayClass?: string;
 }
-const PageTitle: React.FC<PageTitleProps> = ({ title }) => {
+
+const PageTitle: React.FC<PageTitleProps> = ({
+  title,
+  divClass = 'mb-5',
+  displayClass = 'display-5',
+}) => {
   return (
-    <div className="page-title mb-5">
-      <h1 className="display-5 fw-bold">{title}</h1>
+    <div className={`page-title ${divClass}`}>
+      <h1 className={`fw-bold ${displayClass}`}>{title}</h1>
       <GoBack />
     </div>
   );
