5291bf5d9f8efcc4f2c72eeb264ded6bc97898e5
Sandesh Hyoju <sundesz@gmail.com>
2023-07-04 09:48:43 +0300

    Add new config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,8 @@
 export const slugifyOptions = {
   lower: true,
 };
+
+export const BACKEND_BASE_URL = '/api/v1';
+export const BLOG_IMAGE_UPLOAD_URL = `${process.env.VITE_BACKEND_URL}${BACKEND_BASE_URL}/blogs/blogImage`;
+
+export const SUPPORTED_IMAGE_FORMATS = ['image/jpg', 'image/jpeg', 'image/png'];
