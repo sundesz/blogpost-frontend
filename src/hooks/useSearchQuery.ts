6a7f9b3db606844c0f69f6832c73ba9fd7cb0b34
Sandesh Hyoju <sundesz@gmail.com>
2023-07-06 09:11:02 +0300

    Add useSearchQuery hook

--- src/hooks/useSearchQuery.ts
+++ src/hooks/useSearchQuery.ts
@@ -10,8 +10,8 @@ export const useSearchQuery = ({
 }: GetAllRequestQuery) => {
   const [searchParams] = useSearchParams();
   const pageNumber = searchParams.get('page') ?? page;
-  const name = searchParams.get('name') ?? filterName; // table column name
-  const value = searchParams.get('value') ?? filterValue; // table column value
+  const name = searchParams.get('columnName') ?? filterName; // table column name
+  const value = searchParams.get('columnValue') ?? filterValue; // table column value
   const ordBy = searchParams.get('orderBy') ?? orderBy;
   const ordDir = searchParams.get('orderDir') ?? orderDir;
 
