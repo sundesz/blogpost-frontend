0435ad345f5b1341c9a31ce02e9a7f6ca3bf2378
Sandesh Hyoju <sundesz@gmail.com>
2023-07-06 14:16:20 +0300

    Add date

--- src/feature/blogs/ShortBlog.tsx
+++ src/feature/blogs/ShortBlog.tsx
@@ -1,7 +1,7 @@
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import parse from 'html-react-parser';
-
+import { format } from 'date-fns';
 import { Blog } from '../../types';
 
 interface BlogProps {
@@ -12,7 +12,13 @@ const ShortBlog: React.FC<BlogProps> = ({ blog }) => {
   return (
     <Card>
       <Card.Body>
-        <Card.Title>{blog.title}</Card.Title>
+        <Card.Title>
+          <Link to={`/blogs/${blog.slug}`}>{blog.title}</Link>
+        </Card.Title>
+
+        <Card.Text>
+          <small>{format(new Date(blog.updatedAt!), 'dd LLLL yyyy')}</small>
+        </Card.Text>
         <Card.Text>{parse(blog.content)}</Card.Text>
         <Link to={`/blogs/${blog.slug}`}>Read more ...</Link>
       </Card.Body>
