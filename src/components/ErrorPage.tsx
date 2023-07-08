8abc519b93af6d0578f9a69d0d95cd17a316666d
Sandesh Hyoju <sundesz@gmail.com>
2023-07-09 00:11:50 +0300

    Add more condition

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -2,13 +2,19 @@ import { Container } from 'react-bootstrap';
 import { isFetchBaseQueryError } from '../utils/fetchQueryHelper';
 import GoBack from './GoBack';
 import Page404 from './Page404';
+import ServerError from './ServerError';
 
 const ErrorPage = ({ error }: { error: unknown }) => {
   if (
     isFetchBaseQueryError(error) &&
-    (Number(error.status) >= 400 || Number(error.status) < 500)
+    Number(error.status) >= 400 &&
+    Number(error.status) < 500
   ) {
     return <Page404 />;
+  }
+
+  if (isFetchBaseQueryError(error) && Number(error.status) === 500) {
+    return <ServerError />;
   }
 
   let errorMessage = JSON.stringify(error);
